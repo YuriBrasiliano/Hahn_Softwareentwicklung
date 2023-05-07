22a6f21a2416a687a2951d8d31654b686ce19a1a
Yuri <yuribrasiliano37@gmail.com>
2023-05-06 23:34:32 -0300

    adding ValidationBehavior/ FluentValidation

--- Server/Hahn_Softwareentwicklung.Application/DependencyInjection.cs
+++ Server/Hahn_Softwareentwicklung.Application/DependencyInjection.cs
@@ -1,4 +1,7 @@
 
+using System.Reflection;
+using FluentValidation;
+using Hahn_Softwareentwicklung.Application.Common.Behaviors;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,6 +12,8 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(typeof(DependencyInjection).Assembly);
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         
         return services;
     }
